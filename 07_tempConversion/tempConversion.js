8b324308c77e74b25ec14bdb8bc0dffc8017b0cf
gt4yw <27747135+gt4yw@users.noreply.github.com>
2025-02-17 14:37:02 -0500

    Solve 6,7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (typeof temp === 'number') {
+    return Math.round((temp-32)*(5/9)*10,1)/10
+  } else {
+    return "ERROR";
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (typeof temp === 'number') {
+    return Math.round(10*((temp)*(9/5)+32))/10
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
